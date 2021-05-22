e77d3cec9a0a79e82a7cd663fa801710e4de2950
Ambesh Sharm <sharmaambesh101@gmail.com>
2021-05-22 15:38:56 +0530

    Dom parent-child

--- DOM/index.js
+++ DOM/index.js
@@ -1,4 +1,156 @@
+// <!-- // 1--> edit karna text
+// <!-- //    var header = document.getElementById("header-title");
+// //    console.log(header);
+// //    header.textContent="hello"// text change
+// //    header.innerHTML="bye"//--> attention style
+// <!-- // console.log(header.textContent) -->
+// <!-- 
+// // 2 style css
+// // var header = document.getElementById("header-title")
 
-// var header = document.getElementById("hello");
-// console.log(header);
-// console.log("hey")
+// // header.style.borderBottom="solid 2px #000";
+
+// // var mainheader=document.getElementById("main-header");
+// // mainheader.style.borderBottom="solid 2px #000" -->
+
+
+// <!-- // which one we get --loop class use! --
+//     >
+
+// // var items= document.getElementsByClassName("list-group-item");
+// // console.log(items);
+// //    console.log(items[3])
+// //    items[3].textContent="hello";
+// //    items[3].style.fontWeight="bold"; 
+// // items[3].style.background="green"; 
+
+// // for(var i=0; i<items.length;i++){
+// //     items[i].style.background="yellow"   
+// //     // if(i%2==0){
+// //     //     items[i].style.background="green"  
+// //     // }
+// // }
+
+// //TagName -->li, ul, a, h1 ,h3
+// // var li = document.getElementsByTagName('li');
+// // li[1].textContent="bye"
+// // console.log(li);
+// // console.log(li[0]);
+// <!-- 
+// //Query Selector-->
+// // var header =  document.querySelector('#main-header');
+// // header.style.borderBottom="solid 2px #000"
+// // var input= document.querySelector('input');
+// // input.value="hello Khushi Nd aditya"   
+// // var Submit= document.querySelector('input[type="submit"]');
+// // Submit.value="ok";
+
+// //list ke sath query selector
+// // var item =  document.querySelector('.list-group-item:nth-child(2)');
+// // item.style.backgroundColor="red";// single one
+
+// //query selector All
+// //it can grab more than one thing
+
+// // var title = document.querySelectorAll(".title") -->
+
+// // console.log(title[1]); -->
+
+// // var li = document.querySelectorAll('.list-group-item:nth-child(even)');
+// // console.log(li)
+// // var li =document.querySelectorAll('li:nth-child(even)');
+// // console.log(li)
+// // for(var i=0; i< li.length;i++){
+// //     li[i].style.backgroundColor="#f5f5f5"
+// // }
+
+// //traversing DOM
+// // var Itemlist = document.querySelector('#items');
+// // console.log(Itemlist);
+// // // console.log(Itemlist.parentNode)
+// // // console.log(Itemlist.parentNode.parentNode)
+// // // console.log(Itemlist.parentNode.parentNode.parentNode)
+// // // console.log(Itemlist.parentNode.parentNode.parentNode.parentNode)
+// // // console.log(Itemlist.parentNode.parentNode.parentNode.parentNode.parentNode)
+// // console.log(Itemlist.parentElement.parentElement.parentElement.parentElement.parentElement);
+
+// // Question-->
+
+// // function myfun(val, val1){// paramenter
+// //     console.log(val, val1);
+// //     console.log(arguments)
+// // }
+// // myfun(1,2,3,4,5,6,7,8) //---arugrmt
+
+// // most important -->
+// <!-- 
+// // var test="test";
+// // function abc(){
+// //     var test = "inner test";
+// //     console.log(test);
+// //     console.log(this.test);
+// // }
+// // abc(); -->
+
+// <!-- // for(let a=0; a<5; a++){ -->
+// <!-- //     (function(test){
+// //         setTimeout(function(){
+// //             console.log(test);
+// //         },1000)
+// //     })(a);
+// // } -->
+
+// document.write("hello");
+// var hey = document.getElementById("hello");
+// console.log(hey)
+
+
+//**********   21-05-21  **************/
+//childNode
+// var itemlist = document.querySelector("#items");
+// console.log(itemlist.childNodes);
+
+//chlidren
+// specific value can be present
+// var itemlist = document.querySelector("#items");
+// console.log(itemlist.children[1]);
+// itemlist.children[1].style.backgroundColor="red";
+
+// firstchild
+//firstElement Child
+// var itemlist = document.querySelector("#items");
+// console.log(itemlist.firstElementChild);
+// console.log(itemlist.lastElementChild)
+
+//how to track our sibing track
+
+// var itemlist = document.querySelector("#items");
+// console.log(itemlist.nextElementSibling);
+// itemlist.nextElementSibling.textContent="khushi and aditya"
+// console.log(itemlist.previousElementSibling)
+// itemlist.previousElementSibling.textContent="khushi and aditya"
+
+//create element on DOM
+
+//  var newDiv = document.createElement('div');
+// //  console.log(newDiv);
+
+//  newDiv.className="cl";
+// //  console.log(newDiv)
+// newDiv.id='id1';
+// // console.log(newDiv)
+// // how i put the attribut
+// newDiv.setAttribute('name','value');
+// // console.log(newDiv)
+// newDiv.textContent="Hello"
+// console.log(newDiv)
+
+// //add to Html tag
+
+// var cont = document.querySelector('header .container')
+// var h1 =  document.querySelector('header h1');
+// cont.insertBefore(newDiv,h1);
+// newDiv.style.color="black";
+// newDiv.style.fontSize="45px";
+
+// destructering
